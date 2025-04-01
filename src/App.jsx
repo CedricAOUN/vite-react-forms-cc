b90ebae0d043c22003d81caf58a58000d68a20e0
CedricA <cedric.j.aoun@gmail.com>
2025-04-01 17:01:10 +0200

    act 11: used 'yup' for form validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import { Button, Container, Form } from 'react-bootstrap'
 import './App.css'
 import { useForm } from 'react-hook-form';
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required('Le nom est requis')
+    .min(3, 'Le nom doit contenir au moins 3 caractères')
+    .max(15, 'Le nom ne peut pas dépasser 15 caractères'),
+  date: yup
+    .string()
+    .required("La date est requise")
+    .matches(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/, 'Le format doit être jj/mm/AAAA')
+    .test("max-date", "La date ne doit pas être antérieure à la date du jour", (value) => {
+      return new Date(value) >= new Date();
+    }),
+  priority: yup
+    .string()
+    .oneOf(["low", "medium", "high"], "La priorité doit être 'Basse', 'Moyenne' ou 'Elevée"),
+});
 
 function App() { 
   const { register, handleSubmit, reset, formState: { errors }} = useForm({
@@ -9,7 +29,8 @@ function App() {
       date: '',
       priority: 'low',
       isCompleted: false,
-    }
+    },
+    resolver: yupResolver(schema),
   });
     
   const onSubmit = (data) => {
@@ -23,7 +44,7 @@ function App() {
           <Form.Group className="mb-3" controlId="name">
             <Form.Label>Nom</Form.Label>
             <Form.Control 
-              {...register("name", { required: 'Le nom est requis' })} 
+              {...register("name")}
               type="name" 
               placeholder="Entrez votre nom"
             />
@@ -32,15 +53,15 @@ function App() {
           <Form.Group className="mb-3" controlId="date">
             <Form.Label>Date</Form.Label>
             <Form.Control 
-              type="date"
-              {...register("date", { required: 'La date est requise'})}
+              type="text"
+              {...register("date")}
             />
             {errors.date && <p className='text-danger'>{errors.date.message}</p>}
           </Form.Group>
           <Form.Group className="my-3" controlId="priority">
             <Form.Label>Priorité</Form.Label>
             <Form.Select 
-              {...register("priority")} 
+              {...register("priority")}
             >
               <option value="low">Basse</option>
               <option value="medium">Moyenne</option>
