f9dcc79564d2bc7af596f9f34cbb3f479894f4e1
CedricA <cedric.j.aoun@gmail.com>
2025-04-01 15:37:40 +0200

    act 10: implemented useForm hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,49 @@
 import { Button, Container, Form } from 'react-bootstrap'
 import './App.css'
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 function App() { 
-  const [formData, setFormData] = useState({
-    name: '',
-    date: '',
-    priority: 'low',
-    isCompleted: false,
-  });
-
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData({
-      ...formData,
-      [name]: type === "checkbox" ? checked : value,
-    });
+  const { register, handleSubmit } = useForm({});
+    
+  const onSubmit = (data) => {
+    console.log(data);
   }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-  };
-
   return (
     <>
       <Container className='my-4'>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit(onSubmit)}>
           <Form.Group className="mb-3" controlId="name">
             <Form.Label>Nom</Form.Label>
-            <Form.Control type="name" name='name' placeholder="Entrez votre nom" required value={formData.name} onChange={handleChange} />
+            <Form.Control 
+              {...register("name", { required: 'Le nom est requis'})} 
+              type="name" 
+              placeholder="Entrez votre nom"
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="date">
             <Form.Label>Date</Form.Label>
-            <Form.Control type="date" name='date' required value={formData.date} onChange={handleChange}/>
+            <Form.Control 
+              type="date"
+              {...register("date", { required: 'La date est requise'})}
+            />
           </Form.Group>
           <Form.Group className="my-3" controlId="priority">
             <Form.Label>Priorité</Form.Label>
-            <Form.Select aria-label="Default select example" name='priority' value={formData.priority} onChange={handleChange}>
+            <Form.Select 
+              {...register("priority")} 
+              name='priority'
+            >
               <option value="low">Basse</option>
               <option value="medium">Moyenne</option>
               <option value="high">Elevée</option>
             </Form.Select>
           </Form.Group>
           <Form.Group className="my-3" controlId="isCompleted">
-            <Form.Check type="checkbox" name='isCompleted' label="Complet ?" value={formData.isCompleted} onChange={handleChange}/>
+            <Form.Check 
+              {...register("isCompleted")} 
+              type="checkbox" 
+              label="Complet ?"
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
@@ -55,4 +54,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
